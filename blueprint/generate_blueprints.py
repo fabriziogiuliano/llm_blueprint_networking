e31446be1175bd42fe8ba42eb5bbe154530b254f
Fabrizio Giuliano <fabrizio.giuliano@unipa.it>
2024-11-27 11:18:46 +0100

    add simulator blueprints for few-shots

--- blueprint/generate_blueprints.py
+++ blueprint/generate_blueprints.py
@@ -86,6 +86,9 @@ for nDevices in [5, 10, 20, 50, 100, 300, 400, 500 , 1000, 2000]:
                         "address": network_server_addr,
                         "api_token": api_token
                     },
+                    "coverage":{
+                        "radius":radius
+                    },
                     "gateways": [],
                     "devices": []
                 }
